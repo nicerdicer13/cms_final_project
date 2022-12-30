1ae9d78cbeed5b55f1c5dcdfc35ad653a65903e8
Tanja Gruber <tgruber.mmt-b2021@fh-salzburg.ac.at>
2022-12-30 12:17:05 +0100

    wordpress installation

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cp ./src/php/*.* ./public/wp-content/themes/theme_folder/
-cp -R ./src/images ./public/wp-content/themes/theme_folder
-cp -R ./src/fonts ./public/wp-content/themes/theme_folder
-cp -R ./src/favicons ./public/wp-content/themes/theme_folder
-npx sass ./src/sass/style.scss ./public/wp-content/themes/theme_folder/style.css
-npx esbuild ./src/js/main.js --bundle --outfile=./public/wp-content/themes/theme_folder/main.js  --minify
+cp ./src/php/*.* ./public/wp-content/themes/rick/
+cp -R ./src/images ./public/wp-content/themes/rick
+cp -R ./src/fonts ./public/wp-content/themes/rick
+cp -R ./src/favicons ./public/wp-content/themes/rick
+npx sass ./src/sass/style.scss ./public/wp-content/themes/rick/style.css
+npx esbuild ./src/js/main.js --bundle --outfile=./public/wp-content/themes/rick/main.js  --minify
