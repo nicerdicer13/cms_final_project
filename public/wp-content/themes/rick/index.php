671886218500de2d74e12f3e94db5450b0882a25
Tanja Gruber <tgruber.mmt-b2021@fh-salzburg.ac.at>
2023-01-06 19:28:06 +0100

    modularize footer

--- public/wp-content/themes/rick/index.php
+++ public/wp-content/themes/rick/index.php
@@ -144,25 +144,7 @@
             <a class="button">Make me part of your team</a>
         </div>
     </section>
-    <footer>
-        <div>
-            <h2>rick randy</h2>
-            <p>This is a fictional project by Jason Kenny and Tanja Gruber,
-                which was created in the course “Content Management Systems” for the bachelor course MultiMediaTechnology at the Salzburg University of Applied Sciences.</p>
-            <span>2022/23</span>
-        </div>
-        <div class="attributions">
-            <div>
-                <h3>illustrations</h3>
-                <a href="https://storyset.com">https://storyset.com</a>
-            </div>
-            <div>
-                <h3>pictures of rick</h3>
-                <a href="https://unsplash.com/@tilldaling">https://unsplash.com/@tilldaling</a>
-            </div>
-
-        </div>
-    </footer>
+    <?php get_footer(); ?>
     <?php wp_footer(); ?>
 </body>
 
