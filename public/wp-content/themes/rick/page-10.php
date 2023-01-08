d10ab1c400660985e32c934e065e45e35764b895
Jason Christopher Kenny <jkenny.mmt-b2021@fh-salzburg.ac.at>
2023-01-08 17:13:17 +0100

    added image of students for desktop

--- public/wp-content/themes/rick/page-10.php
+++ public/wp-content/themes/rick/page-10.php
@@ -132,8 +132,7 @@
         </div>
     </section>
     <section id="unlock">
-        <h2>Unlock your company's full potential.</h2>
-        <!-- <a class="button" href="#">Contact me now</a> -->
+        <h2>Unlock your company's full potential. Contact me now.</h2>
         <div class="contact-form">
             <?php echo do_shortcode('[contact-form-7 id="12"]'); ?>
         </div>
