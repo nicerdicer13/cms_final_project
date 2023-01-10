62c435b35f618157bd4fb42a7ca1478d9685c33d
Tanja Gruber <tgruber.mmt-b2021@fh-salzburg.ac.at>
2023-01-10 08:57:32 +0100

    add: favicon

--- src/php/index.php
+++ src/php/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rick Randy</title>
+    <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri(); ?>/favicons/favicon.svg"/>
     <?php wp_head(); ?>
 </head>
 
--- src/php/page-10.php
+++ src/php/page-10.php
@@ -8,6 +8,7 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri(); ?>/favicons/favicon.svg"/>
     <?php wp_head(); ?>
 </head>
 <?php wp_body_open(); ?>
